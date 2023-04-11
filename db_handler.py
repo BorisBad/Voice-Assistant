b4f0aa2fe33e932c9a609cdad2bb29a98e6ffc68
Boris Baishev <baishev.boris@mail.ru>
2023-04-11 15:07:35 +0500

    first commit, added CRUD controller

---
+++ db_handler.py
@@ -0,0 +1,92 @@
+import sqlite3
+
+#TO_DO
+#Check for possibelities of sql injections!
+#Make try except statement
+#Check for data validity
+#Add 'where' statements in CRUD
+#Probably function for cleaning table_name and such from possible sql injection
+#END_TO_DO
+
+#region Work_with_connection
+
+def create_or_connect_to_db(db_name: str):
+    db = sqlite3.connect(db_name)
+    return db
+
+def close_connection(db: sqlite3.Connection):
+    db.close()
+    # try:
+    #     db.close()
+    # except Exception as e:
+    #     print("Something went wrong"+'\n'+str(e))
+
+def update_db(db: sqlite3.Connection):
+    db.commit()
+
+def create_cursor(db: sqlite3.Connection):
+    cur = db.cursor()
+    return cur
+
+def delete_cursor(cur: sqlite3.Cursor):
+    cur.close()
+
+#endregion
+
+
+#region Work_with_tables
+
+#CREATE IF NOT EXISTS?
+def create_table(db: sqlite3.Connection, cur: sqlite3.Cursor, table_name: str, fields:list):
+    cur.execute("""CREATE TABLE IF NOT EXISTS {} (
+                    id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+                    awaited_request text NOT NULL,
+                    module text NOT NULL,
+                    command text NOT NULL,
+                    last_modified date NOT NULL
+                    )
+                    """.format(table_name))
+    update_db(db)
+
+def delete_table(db: sqlite3.Connection, cur: sqlite3.Cursor, table_name: str):
+    cur.execute("""DROP TABLE {};""".format(table_name))
+    update_db(db)
+
+#endregion
+
+
+#region CRUD
+
+#Create
+def insert_into(db: sqlite3.Connection, cur: sqlite3.Cursor, table_name: str, values: list):
+    #request, module, command, last_mod
+    cur.execute(
+                """INSERT INTO {} VALUES (?,?,?,?,?);""".format(table_name),
+                (None, values[0], values[1], values[2], values[3])
+                )
+    update_db(db)
+
+#Read (all)
+def read_from(db: sqlite3.Connection, cur: sqlite3.Cursor, table_name: str):
+    cur.execute("""SELECT * FROM {};""".format(table_name))
+    items = cur.fetchall()
+    return items
+
+#Update (by id)
+def update_in(db: sqlite3.Connection, cur: sqlite3.Cursor, table_name: str, values: list):
+    cur.execute(
+    """UPDATE {} SET {} = ? WHERE id = ?;""".format(table_name, values[0]),
+    (values[1], values[2]))
+    update_db(db)
+
+#Delete
+def delete_from(db: sqlite3.Connection, cur: sqlite3.Cursor, table_name: str, values:list):
+    cur.execute(
+    """ DELETE FROM {}
+    WHERE id = {} ;
+    """.format(table_name, values[0]))
+    update_db(db)
+
+#endregion
+
+
---
+++ main.py
@@ -0,0 +1,84 @@
+import os
+import sys
+import speech_recognition as sr
+import pyaudio
+import threading
+import datetime
+import time
+import db_handler as dbh
+import subprocess
+
+def mycallback(recognizer, audio):
+    text = recognizer.recognize_google(audio, language="ru-RU")
+    print(text)
+    #print()
+#recognize_google(self, audio_data, key=None, language="en-US", pfilter=0, show_all=False, with_confidence=False):
+
+def main():
+#region WTF?
+    db_name = 'test.db'
+    db = dbh.create_or_connect_to_db(db_name)
+    c = dbh.create_cursor(db)
+
+    dbh.create_table(db,c,'Commands',[1,2])
+    print(dbh.read_from(db,c,'Commands'))
+    print('----------------')
+    #request, module, command, last_mod
+    dbh.insert_into(db,c,'Commands',["Скажи привет","cmd","echo Здарова",datetime.datetime.now()])
+    print(dbh.read_from(db,c,'Commands'))
+    print('----------------')
+    #SET ? = ? WHERE id = ? 
+    dbh.update_in(db,c,'Commands', ["command", "echo Привет", 1])
+    print(dbh.read_from(db,c,'Commands'))
+
+
+    items = dbh.read_from(db,c,'Commands')
+    # print(items)
+    # print(items[0])
+    # print(items[0][3])
+    # print("Print 'stop' to end")
+    # while input() != "stop":
+    #     print("Print 'stop' to end")
+    #rez = subprocess.run(["cd"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #print(rez.stdout.decode())
+    
+    os.system('cmd /c '+ items[0][3])
+    #os.system('cmd /c ' + "cd")
+
+
+
+
+    
+    print('----------------')
+    dbh.delete_from(db,c,'Commands',[1])
+    print(dbh.read_from(db,c,'Commands'))
+    print('----------------')
+
+    print("Print 'stop' to end")
+    while input() != "stop":
+        print("Print 'stop' to end")
+
+    dbh.delete_table(db,c,'Commands')
+    dbh.delete_cursor(c)
+    dbh.close_connection(db)
+#endregion
+    
+    
+    
+    
+    # mic = sr.Microphone()
+    # recognizer = sr.Recognizer()
+    # is_stop = True
+
+    # while is_stop:
+    #     # with mic as source:
+    #     #     recognizer.adjust_for_ambient_noise(source)
+    #     text = recognizer.listen(mic)
+    #     #stopper = recognizer.listen_in_background(mic, mycallback)
+    #     time.sleep(3)
+    #     #stopper()
+    #     is_stop = False
+    #     print(text)
+            
+#if __name__ == '__main__':
+main()
